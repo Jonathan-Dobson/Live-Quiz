884903cf86a1760545d1445cf090ea03ff179fcb
fromjdobson <fromjdobson@gmail.com>
2019-09-06 09:59:14 -0600

    add put and delete

--- server/routes/questionRoute.js
+++ server/routes/questionRoute.js
@@ -1,20 +1,22 @@
-const Question = require('../models/Question')
+const Questions = require('../models/Question')
 const express = require('express')
 const questionRouter = express.Router()
 
 questionRouter.route('/')
     .get((req,res)=>{
-        Question.find((err,question)=>{
+        Questions.find((err,question)=>{
             if(err) return res.status(500).send(err)
             return res.status(200).send(question) 
         })
     })
     .post((req,res)=>{
-        const newQuestion = new Question(req.body)
+        const newQuestion = new Questions(req.body)
         newQuestion.save(err=>{
             if(err) return res.status(500).send(err)
             return res.status(201).send(newQuestion) 
         })
     })
+
+
 
 module.exports = questionRouter
