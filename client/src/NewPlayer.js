63f75d0906d71c1a2c7740dd80486d9fa08250f9
Micheal Shanahan <m_shanahan@hotmail.com>
2019-09-06 11:03:20 -0600

    Added Proveder File

--- client/src/NewPlayer.js
+++ client/src/NewPlayer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withQuestion } from './QuestionProvider'
 
-const NewPlayer = () => {
-    return(
-        <div>Hello</div>
-    )
+class NewPlayer extends Component {
+    constructor(){
+        super()
+        this.state ={}
+    }
+    render() {
+        return(
+            <div>Hello
+                <button onClick = {() => console.log(this.props)} >ConsoleLogState</button>
+            </div>
+        )
+    }
 }
 
-export default NewPlayer
+export default withQuestion(NewPlayer)
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+
+import QuestionProvider from './QuestionProvider'
 import App from './App'
 
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <QuestionProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </QuestionProvider>
     , document.getElementById('root')
 )
