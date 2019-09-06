285d78983a23564fb3e47286296878955b368b80
fromjdobson <fromjdobson@gmail.com>
2019-09-06 10:03:53 -0600

    add put and delete

--- server/routes/questionRoute.js
+++ server/routes/questionRoute.js
@@ -16,7 +16,29 @@ questionRouter.route('/')
             return res.status(201).send(newQuestion) 
         })
     })
+questionRouter.route('/:id')
+    .put((req,res)=>{
+        Questions.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true},
+            (err, question) => {
+                if (err) return res.status(500).send(err);
+                return res.send(question);
+            }
+        )
+    })
+    .delete((req,res)=>{
+        Questions.findByIdAndRemove(req.params.id, (err, person) => {
+            if (err) return res.status(500).send(err);
+            const response = {
+                message: "successfully deleted",
+                id: person._id
+            };
+            return res.status(200).send(response);
+        });
 
+    })
 
 
 module.exports = questionRouter
