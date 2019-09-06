8f9e9bad5fba168a9065c04ad7f3f32078bb19fe
Micheal Shanahan <m_shanahan@hotmail.com>
2019-09-05 20:35:31 -0600

    Index Folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom' 
+import NewPlayer from './NewPlayer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <Switch>
+            <Route exact path = '/' component = { NewPlayer } />
+    
+        </Switch>
+    )
 }
 
-export default App;
+export default App
