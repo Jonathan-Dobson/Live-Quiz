0cfd62132a054434e1e10069c6e33f32d0d70a63
fromjdobson <fromjdobson@gmail.com>
2019-09-06 12:27:13 -0600

    made route return all categories

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -2,9 +2,18 @@ const Questions = require('../models/Question')
 const express = require('express')
 const categoryRouter = express.Router()
 
+categoryRouter.route('/')
+    .get((req,res)=>{
+        Questions.find((err,question)=>{
+            if(err) return res.status(500).send(err)
+            let categories = question.reduce((a,b)=>a.some(c=>b.category===c) ? a : [...a,b.category],[])
+            console.log(categories);
+            return res.status(200).send(categories)
+        })
+    })
+
 categoryRouter.route('/:category')
     .get((req,res)=>{
-        console.log(req.params);
         Questions.find({category: req.params.category },(err,question)=>{
             if(err) return res.status(500).send(err)
             return res.status(200).send(question) 
